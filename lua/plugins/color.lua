236107c90331e6a166e0b87a390192eed75777b2
josh <joshua.smotritsky@gmail.com>
2025-06-07 11:25:08 -0400

    removes themes and plugins

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -4,49 +4,10 @@ return {
 		config = function() end,
 	},
 	{
-		"datsfilipe/vesper.nvim",
-		config = function()
-			require("vesper").setup({
-				-- optional configuration here
-			})
-		end,
-	},
-	{
-		"shaunsingh/nord.nvim",
-		config = function()
-			-- NOTE: you do not need to call setup if you don't want to.
-			require("gitsigns").setup({
-				-- optional configuration here
-			})
-		end,
-	},
-	{
 		"Mofiqul/vscode.nvim",
 		config = function()
 			-- NOTE: you do not need to call setup if you don't want to.
 			require("vscode").setup({
-				-- optional configuration here
-			})
-		end,
-	},
-	{
-		"xero/miasma.nvim",
-		config = function() end,
-	},
-	{
-		"miikanissi/modus-themes.nvim",
-		config = function()
-			-- NOTE: you do not need to call setup if you don't want to.
-			require("modus-themes").setup({
-				-- optional configuration here
-			})
-		end,
-	},
-	{
-		"vague2k/vague.nvim",
-		config = function()
-			-- NOTE: you do not need to call setup if you don't want to.
-			require("vague").setup({
 				-- optional configuration here
 			})
 		end,
@@ -64,27 +25,14 @@ return {
 		config = function() end,
 	},
 	{
-		"Koalhack/darcubox-nvim",
-		config = function() end,
-	},
-	{
 		"rebelot/kanagawa.nvim",
 		name = "kanagawa",
-		config = function() end,
-	},
-	{
-		"bluz71/vim-moonfly-colors",
-		name = "moonfly",
 		config = function() end,
 	},
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
 		config = function() end,
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		config = true,
 	},
 	{
 		"sainnhe/gruvbox-material",
@@ -94,10 +42,6 @@ return {
 			vim.g.gruvbox_material_enable_italic = true
 			vim.g.gruvbox_material_background = "soft"
 		end,
-	},
-	{
-		"projekt0n/github-nvim-theme",
-		config = function() end,
 	},
 	{
 		"zenbones-theme/zenbones.nvim",
