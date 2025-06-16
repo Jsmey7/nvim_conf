1b8e3776d1063ecba59e32532930d270855e3d71
josh <joshua.smotritsky@gmail.com>
2025-06-16 12:44:13 -0400

    added highlight comment:

--- lua/opts.lua
+++ lua/opts.lua
@@ -5,6 +5,7 @@ vim.g.have_nerd_font = true
 vim.opt.colorcolumn = "120"
 vim.opt.signcolumn = "yes"
 vim.opt.foldcolumn = "1"
+vim.opt.foldmethod = "manual"
 
 -- settings
 vim.o.number = true
---
+++ lua/plugins/todo_comments.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"folke/todo-comments.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			keywords = {
+				TODO = { icon = " ", color = "info" },
+				HACK = { icon = " ", color = "warning" },
+				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+				PERF = { icon = " ", color = "#7C3AED", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+				NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+				TEST = { icon = "⏲ ", color = "#FF00FF", alt = { "TESTING", "PASSED", "FAILED" } },
+			},
+			colors = {
+				error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+				warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+				info = { "DiagnosticInfo", "#2563EB" },
+				hint = { "DiagnosticHint", "#10B981" },
+				default = { "Identifier", "#7C3AED" },
+				test = { "Identifier", "#FF00FF" },
+			},
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+}
